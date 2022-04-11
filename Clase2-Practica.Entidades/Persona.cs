82ab90ed7d07db304893dd3cafcdc5cdf1271ef7
LeaRamos <temple805@gmail.com>
2022-04-11 00:14:52 -0300

    controllerImc

---
+++ Clase2-Practica.Entidades/Persona.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Clase2_Practica.Entidades
+{
+    public class Persona
+    {
+        public int Id { get; set; }
+        public float altura { get; set; }
+        public float peso { get; set; }
+        public float IMC { get; set; }
+
+    }
+}
---
+++ Clase2-Practica.Web/Controllers/CalculadorIMCController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Clase2_Practica.Entidades;
+
+namespace Clase2_Practica.Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculadorIMCController : ControllerBase
+    {
+        private readonly Persona _persona;
+        private readonly  _personas;
+
+        public CalculadorIMCController(Persona persona) {           
+            _persona = persona;
+        }
+        
+
+        [HttpGet]
+        public float CalcularIMCDeUnIndividuo(float peso,float altura)
+        {
+            float imc = 0f;
+
+            if (peso > 0 && altura > 0) {
+                imc =  peso /(altura* altura);
+            }
+            return imc;
+        }
+    }
+}
